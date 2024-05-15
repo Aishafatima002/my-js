1499938f478f94defd1f01d9678eabff1e018aa0
Aisha <164768280+Aishafatima002@users.noreply.github.com>
2024-05-15 15:21:21 +0500

    updayte

---
+++ app.js
@@ -0,0 +1,74 @@
+//class activity
+//question n answer
+//Q1
+// let age=prompt("Enter your age");
+// if(age>18){
+//     console.log("you are eligible for the vote");
+// }
+// else if(age<18){
+//     console.log("you are not eligible for the vote")
+// };
+//Q2
+// let temperature=prompt("whats THE Temperature is?");
+// if(temperature>25){
+//     console.log("it is hot day")
+// }
+// else if(temperature<25){
+//     console.log("it is normal day")
+// };
+
+//Q3
+// let score=prompt("Enter your score");
+// if(score>60){
+//     console.log("you passed the test");
+// }
+// else if (score ==60){
+//     console.log("you are passed");
+
+// }
+// else {
+//     console.log("you are failed")
+// };
+
+//Q4
+// let raining=prompt("is it raining? type true or false");
+// if(raining=="true"){
+//     console.log("Remember to bring an umbrella.");
+// }
+// else if(raining!=="false"){
+//     console.log("not bring umbrella")
+// };
+
+//Q5
+// let age=prompt("Enter your age");
+// if(age>18){
+//     console.log("you  are adult");
+// }
+// else if (age ==60){
+//     console.log("you are adult");
+
+// }
+// else {
+//     console.log("you are minor")
+// };
+ 
+//Q5
+// let number=prompt("Input a number");
+// if(number<7){
+//     console.log("It is positive");
+// }
+// else if(number==7){
+//     console.log("IT is zero")
+// }
+// else{
+//     console.log("It is negative")
+// }
+
+// // Q4
+// let raining=true
+// if(raining==true){
+//     console.log("remember to bring umbrella")
+// }
+// else{
+//     console.log("enjoy the sunny weather")
+// }
